5eaef77961fb309de11d39b63d6415f8428a1b36
Syed shahid shabir qadri <53717633+shahidshabir055@users.noreply.github.com>
2022-02-09 10:52:06 +0530

    changes

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -9,14 +9,24 @@ import {ErrorStateMatcher} from '@angular/material/core';
 export class RegisterComponent implements OnInit {
 
   constructor() { }
-
+  registerDisabled = false;
 
   ngOnInit(): void {
   }
   fullName = new FormControl('', Validators.required)
   email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('', Validators.required);
-  repeatPassword = new FormControl('', Validators.required)
+  password = new FormControl('', [Validators.required, Validators.minLength(8)]);
+  repeatPassword = new FormControl('', [Validators.required, Validators.minLength(8)]);
+  comparePassword(){
+    console.log(this.password.value)
+    console.log(this.repeatPassword.value)
+    if(this.password.value == this.repeatPassword.value){
+      this.registerDisabled=false;
+    }
+    else{
+      this.registerDisabled=true;
+    }
+  }
 
 
 
